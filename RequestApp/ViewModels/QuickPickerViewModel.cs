da9c029dbcf75727cd6e966fe4bc500b733ae598
e3bauer <e3bauer@GFONG21A>
2026-05-15 16:15:35 -0400

    New view for quickly selecting a value from a list

---
+++ RequestApp/ViewModels/QuickPickerViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using DocumentFormat.OpenXml.Wordprocessing;
+using MvvmLib.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestApp.ViewModels
+{
+    public partial class QuickPickerViewModel : WorkspaceViewModel
+    {
+        public string Label { get; }
+        public IEnumerable<object> ItemList { get; }
+        public object SelectedItem { get; set; }
+
+        public QuickPickerViewModel(string label, IEnumerable<object> items)
+        {
+            Label = label;
+            ItemList = items;
+        }
+
+        [RelayCommand]
+        private void OK()
+        {
+            OnRequestClose(true);
+        }
+
+        [RelayCommand]
+        private void Cancel()
+        {
+            OnRequestClose(false);
+        }
+    }
+}
